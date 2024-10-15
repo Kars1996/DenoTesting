9cd82f6c6f7c0dc03c714cfd4fc1f10b3eba0ed6
Kars <k@kars.bio>
2024-10-15 09:06:07 +0100

    chore: STD Library tests

--- main.ts
+++ main.ts
@@ -1,18 +1,12 @@
-function _addBinary(one: string, two: string): string {
-    let result: string = "";
-    let carry: number = 0;
-    let i = one.length - 1;
-    let j = two.length - 1;
+const enc = new TextEncoder();
 
-    while (i >= 0 || j >= 0 || carry > 0) {
-        const bit1 = i >= 0 ? parseInt(one[i]) : 0;
-        const bit2 = j >= 0 ? parseInt(two[j]) : 0;
-
-        const sum = bit1 + bit2 + carry;
-        result = (sum % 2).toString + result;
-        carry = sum / 2;
-        i -= 1;
-        j -= 1;
-    }
-    return result;
+function hashString(data: string): string {
+    return enc.encode(data).toString();
 }
+
+// Usage
+const data = "Hello, world!";
+const hashedData = hashString(data);
+console.log("Hashed data:", hashedData);
+console.log("Hashed data: (again)", hashString(data));
+
