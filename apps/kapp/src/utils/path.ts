ee968780d6c2a3c820dae832dc7eebbb7608f6e9
Kars <k@kars.bio>
2024-10-20 23:58:52 +0100

    feat: Path validation

---
+++ apps/kapp/src/utils/path.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env deno
+
+import { resolve } from "@std/path";
+import { ensureDir } from "@std/fs";
+import _UI from "../ui/ui.ts";
+
+export default class PathUtils {
+    public async resolvePath(path: string): Promise<string> {
+        let absPath: string;
+        if (path === "/" || path === ".") {
+            absPath = Deno.cwd();
+        } else {
+            absPath = resolve(Deno.cwd(), path);
+        }
+
+        try {
+            await Deno.stat(absPath);
+        } catch (error) {
+            if (error instanceof Deno.errors.NotFound) {
+                await ensureDir(absPath);
+                // UI.print(`Created Dir`)
+            } else {
+                throw error;
+            }
+        }
+        return absPath;
+    }
+}
