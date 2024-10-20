20f32b29b1b18ccfd5d5ce18d39efc5b58d2c113
Kars <k@kars.bio>
2024-10-20 23:58:25 +0100

    feat: General Changes

--- apps/kapp/src/main.ts
+++ apps/kapp/src/main.ts
@@ -1,6 +1,32 @@
 #!/usr/bin/env deno
+import UI from "./ui/ui.ts";
+import Prompts from "./ui/prompts.ts";
+import Download from "./utils/download.ts";
+import templateOptions, { icons, templatePaths } from "./lib/consts.ts";
 
 // TODO: Main UI functionality
+const isDev: boolean = Deno.args.includes("--dev");
+
+function earlyExit(value: unknown): asserts value is NonNullable<typeof value> {
+    if (value === undefined || value === null) {
+        UI.print("Exiting Early");
+        UI.footer();
+        Deno.exit(0);
+    }
+}
+
 if (import.meta.main) {
-    console.log("WIP")
+    UI.header(isDev);
+    UI.print("Welcome to the Deno Testing Framework!");
+    const templateCategory = await Prompts.choice(
+        "Select a template category",
+        Object.keys(templateOptions) as (keyof typeof templateOptions)[]
+    );
+    earlyExit(templateCategory)
+    const template = await Prompts.choice(
+        `${icons[templateCategory]} Select a template`,
+        templateOptions[templateCategory]
+    );
+    earlyExit(template)
+
 }
